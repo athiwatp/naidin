4652d5a809cb26700e11985c2153a43e210cce57
noomerzx <noomerzx@gmail.com>
2016-08-29 17:34:49 +0700

    refacted some logic

--- js/app.js
+++ js/app.js
@@ -91,34 +91,44 @@ new Vue({
 // calculate summary for all item in cart
 function calculateSummary(refCart){
     var cartList = JSON.parse(JSON.stringify(refCart));
-
+    var orderCount = cartList.length;
     // just in case
     // if it's has non harry potter book catch it here
     // loop to check if item[index].id is not harry then remove it from cartList and sum all amount
     // then return calculateSummary(cartList) + 100*sumAmount and when carList have only harry potter it'll' pass this section to another promotion below
 
+    // old
     // harry potter promotion below
-    if(cartList.length == 2){
+    // if(cartList.length == 2){
+    //     cartList = cutAllItem(cartList);
+    //     return calculateSummary(cartList) + 100*2*0.9;
+    // } else if(cartList.length == 3){
+    //     cartList = cutAllItem(cartList);
+    //     return calculateSummary(cartList) + 100*3*0.8;
+    // } else if(cartList.length == 4){
+    //     cartList = cutAllItem(cartList);
+    //     return calculateSummary(cartList) + 100*4*0.7;
+    // } else if(cartList.length == 5){
+    //     cartList = cutAllItem(cartList);
+    //     return calculateSummary(cartList) + 100*5*0.6;
+    // } else if(cartList.length == 6){
+    //     cartList = cutAllItem(cartList);
+    //     return calculateSummary(cartList) + 100*6*0.5;
+    // } else if(cartList.length == 7){
+    //     cartList = cutAllItem(cartList);
+    //     return calculateSummary(cartList) + 100*7*0.4;
+    // } else if (cartList.length == 1) {
+    //     cartList = cutAllItem(cartList);
+    //     return calculateSummary(cartList) + 100;
+    // } else {
+    //     return 0;
+    // }
+
+    // new
+    // harry potter promotion below
+    if(orderCount > 0){
         cartList = cutAllItem(cartList);
-        return calculateSummary(cartList) + 100*2*0.9;
-    } else if(cartList.length == 3){
-        cartList = cutAllItem(cartList);
-        return calculateSummary(cartList) + 100*3*0.8;
-    } else if(cartList.length == 4){
-        cartList = cutAllItem(cartList);
-        return calculateSummary(cartList) + 100*4*0.7;
-    } else if(cartList.length == 5){
-        cartList = cutAllItem(cartList);
-        return calculateSummary(cartList) + 100*5*0.6;
-    } else if(cartList.length == 6){
-        cartList = cutAllItem(cartList);
-        return calculateSummary(cartList) + 100*6*0.5;
-    } else if(cartList.length == 7){
-        cartList = cutAllItem(cartList);
-        return calculateSummary(cartList) + 100*7*0.4;
-    } else if (cartList.length == 1) {
-        cartList = cutAllItem(cartList);
-        return calculateSummary(cartList) + 100;
+        return calculateSummary(cartList) + (orderCount == 1 ? 100 : 100*orderCount*(1-((orderCount-1)/10)));
     } else {
         return 0;
     }
@@ -130,13 +140,25 @@ function calculateSummary(refCart){
 // Cut all item amount by 1 if the amount is zero remove the item from cartList
 function cutAllItem(refCart){
     var cartList = JSON.parse(JSON.stringify(refCart));
+
+    // old
+    // for(var i = 0 ; i < cartList.length; i++){
+    //     if(cartList[i].amount > 0){
+    //         cartList[i].amount -= 1;
+    //         if(cartList[i].amount == 0){
+    //             cartList.splice(i, 1);
+    //             i--;
+    //         }
+    //     } 
+    // }
+
+    // new
     for(var i = 0 ; i < cartList.length; i++){
-        if(cartList[i].amount > 0){
+        if(cartList[i].amount > 1){
             cartList[i].amount -= 1;
-            if(cartList[i].amount == 0){
-                cartList.splice(i, 1);
-                i--;
-            }
+        } else {
+            cartList.splice(i, 1);
+            i--;
         } 
     }
     return cartList;
